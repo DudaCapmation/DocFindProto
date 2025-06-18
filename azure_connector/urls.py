9f70d14d05ff9615e8db5ecf7c077a20e811bd72
Eduarda <maria.santana@capmation.com>
2025-06-18 11:37:32 -0300

    Fixed a minor bug.

--- azure_connector/urls.py
+++ azure_connector/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import azure_create, azure_delete, azure_get, azure_get_url
+from .views import azure_create_or_update, azure_delete, azure_get, azure_get_url
 
 urlpatterns = [
-    path('create/', azure_create),
+    path('create/', azure_create_or_update),
     path('delete/', azure_delete),
     path('get/', azure_get),
     path('get-url/', azure_get_url),
